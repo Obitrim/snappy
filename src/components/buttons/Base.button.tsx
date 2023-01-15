264cbc5d121360eb19c2d6a87cc255db062dff9d
Obitrim <pevangelist516@gmail.com>
2023-01-15 13:17:24 +0000

    feat: improve reusability

--- src/components/buttons/Base.button.tsx
+++ src/components/buttons/Base.button.tsx
@@ -3,28 +3,38 @@ import clsx from "clsx";
 
 type ComponentProps = {
   text?: string;
+  size?: "large" | "small";
   variant?: "outline" | "primary" | "ghost";
 } & ComponentPropsWithRef<"button">;
 
 const BaseButton = ({
   text,
   variant = "primary",
+  size = "small",
   className,
   children,
 }: ComponentProps) => {
   return (
     <button
       className={clsx(
-        [" text-xl font-semibold", "px-4 py-3"],
+        "relative overflow-hidden",
+        "mix-blend-difference",
         [
-          variant === "primary" && ["border border-black bg-black text-white"],
+          size === "small" ? "px-3 py-1.5" : "px-4 py-3",
+          variant === "primary" && [
+            "border border-black bg-black text-white",
+            "border border-black",
+
+            "z-10 before:absolute before:left-0 before:top-0 before:h-full before:w-0 before:transition-all before:duration-200",
+            "before:bg-white hover:text-black  hover:before:w-full",
+          ],
           variant === "ghost" && [],
           variant === "outline" && [],
         ],
         className
       )}
     >
-      {text || children}
+      <span className="relative z-20">{text || children}</span>
     </button>
   );
 };
